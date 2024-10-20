b1f6200166d5c7aebe2698e3b10d3ec586518519
Ivan <i.shabashev@apptimizm.pro>
2024-10-20 23:53:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import asyncio
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 from aiogram import Bot, Dispatcher, F, Router
 from aiogram.types import Message
-from aiogram.filters import CommandStart, Command
+
+
 def checker_map(map):
     count = 0
     s = []
@@ -27,11 +28,15 @@ def checker_map(map):
         if i.count("o") == 3:
             return 2
     return 0
+
+
 def restart_game():
     global game_map
     global player
     game_map = [["#", "#", "#"], ["#", "#", "#"], ["#", "#", "#"]]
     player = random.choice(['x', 'o'])
+
+
 player: str = None
 game_map = [["#", "#", "#"],
             ["#", "#", "#"],
@@ -40,44 +45,44 @@ kb1 = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='0'), KeyboardButton(te
                                     [KeyboardButton(text='3'), KeyboardButton(text='4'), KeyboardButton(text='5')],
                                     [KeyboardButton(text='6'), KeyboardButton(text='7'), KeyboardButton(text='8')],
                                     [KeyboardButton(text='Начать заново')]],
-                                    resize_keyboard=True)
+                          resize_keyboard=True)
 
 bot = Bot(token='7639555111:AAFAvfiO18kgl23zHzwHKn38tWac9frIJcw')
 ds = Dispatcher()
+
 
 @ds.message()
 async def start(message: Message):
     global game_map
     global player
-    k = game_map[0].count('#') + game_map[1].count('#') + game_map[2].count('#')
     if message.text == "/start":
         restart_game()
-        await message.answer(f'Игру начинает {player} \n {game_map[0]} \n {game_map[1]} \n {game_map[2]}', reply_markup=kb1)
-    if k == 0:
-        restart_game()
-        await message.answer('Ничья')
-        await message.answer('Игра начинается заново')
-        await message.answer(f'Игру начинает {player}\n {game_map[0]} \n {game_map[1]} \n {game_map[2]}')
-
-        return None
+        await message.answer(f'Игру начинает {player} \n {game_map[0]} \n {game_map[1]} \n {game_map[2]}',
+                             reply_markup=kb1)
     if str(message.text) == 'Начать заново':
         restart_game()
         await message.answer('Игра начинается заново')
         await message.answer(f'Игру начинает {player}\n {game_map[0]} \n {game_map[1]} \n {game_map[2]}')
         return None
     if message.text.isdigit():
-        k = game_map[0].count('#')+game_map[1].count('#')+game_map[2].count('#')
         if game_map[int(message.text) // 3][int(message.text) % 3] == '#':
             game_map[int(message.text) // 3][int(message.text) % 3] = player
-            if checker_map(game_map) == 1:
+            k = game_map[0].count('#') + game_map[1].count('#') + game_map[2].count('#')
+            if k == 0:
+                await message.answer(f'Ничья \n {game_map[0]}\n {game_map[1]}\n {game_map[2]}')
                 restart_game()
-                await message.answer('Победа Крестика')
+                await message.answer('Игра начинается заново')
+                await message.answer(f'Игру начинает {player}\n {game_map[0]} \n {game_map[1]} \n {game_map[2]}')
+                return None
+            if checker_map(game_map) == 1:
+                await message.answer(f'Победа Крестика \n {game_map[0]}\n {game_map[1]}\n {game_map[2]}')
+                restart_game()
                 await message.answer('Игра начинается заново')
                 await message.answer(f'Игру начинает {player}\n {game_map[0]} \n {game_map[1]} \n {game_map[2]}')
                 return None
             if checker_map(game_map) == 2:
+                await message.answer(f'Победа Нолика \n {game_map[0]}\n {game_map[1]}\n {game_map[2]}')
                 restart_game()
-                await message.answer('Победа Нолика')
                 await message.answer('Игра начинается заново')
                 await message.answer(f'Игру начинает {player}\n {game_map[0]} \n {game_map[1]} \n {game_map[2]}')
                 return None
@@ -92,4 +97,6 @@ async def start(message: Message):
 
 async def main():
     await ds.start_polling(bot)
+
+
 asyncio.run(main())
